ebf66428535e0a6e38f6e54409e61cd8b97c41ff
unknown <joel.epstein2@gmail.com>
2017-09-23 16:41:07 -0700

    fixed stupid mistakes in fp.js

--- lab02-joel/lib/fp.js
+++ lab02-joel/lib/fp.js
@@ -4,9 +4,9 @@ const fp = module.exports = {};
 
 fp.map = (list, callback) => Array.prototype.map.call(list, callback);
 
-fp.filter (list, callback) => Array.prototype.filter.call(list, callback);
+fp.filter = (list, callback) => Array.prototype.filter.call(list, callback);
 
-fp.reduce (list, args) => Array.prototype.reduce.apply(list, args);
+fp.reduce = (list, args) => Array.prototype.reduce.apply(list, args);
 
 fp.concat = (list1, list2) => Array.prototype.concat.call(list1, list2);
 
